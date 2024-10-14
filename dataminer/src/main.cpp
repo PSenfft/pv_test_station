de170a1c3a9ed0dd16736af16e1e8128cc73630c
Philipp <philipp.senfft@gmail.com>
2024-10-14 18:45:12 +0200

    working on wait or sleep method

--- dataminer/src/main.cpp
+++ dataminer/src/main.cpp
@@ -1,5 +1,7 @@
 #include "main.h"
 
+RTC_DS3231 rtc;
+
 struct Data{
   uint16_t voltage: 12;
   uint16_t current: 12;
@@ -10,10 +12,63 @@ struct Data{
 
 Flags flags = {0, 0, 0};
 
+int last_millis = 0;
+
+// --- iunterrupts ---
+void pps_isr(){
+  flags.pps_interrupt = 1;
+}
+
+void measure_everything(){
+  //TODO read voltage
+  //TODO read current
+  //TODO read temp_env
+  //TODO read temp_panel
+  //TODO read timestamp
+}
+
+void check_timings(){
+  // if no interrupt from PPS signal leave function
+  if (!flags.pps_interrupt){ return;}
+
+  read_gps();
+
+  //proof if gps time is valid otherwise set flag to use rtc time
+  if(get_unix_time_from_gps() < POSSIBLE_TIME){
+    flags.use_gps_time = 0; // GPS time is not valid use RTC time
+  }else{
+    flags.use_gps_time = 1; // GPS time is valid use GPS time
+  }
+
+  measure_everything();
+  //TODO set wachdog timer
+
+  flags.pps_interrupt = 0;
+}
+
+void init_rtc(){
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    while (1) delay(10);
+  }
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+}
+
 void setup() {
-  // put your setup code here, to run once:
+  //Pin setup
+  pinMode(PPS_PIN, INPUT);
+  pinMode(VOLTAGE_METER_PIN, INPUT);
+  pinMode(CURRENT_METER_PIN, INPUT);
+  attachInterrupt(PPS_PIN, pps_isr, RISING);
+
+  //USB Serial
   Serial.begin(9600);
+
+  init_rtc();
   init_temp();
+  init_gps();
+  init_lora();
 }
 
 void loop() {
@@ -21,11 +76,19 @@ void loop() {
   Serial.println("Hello World");
 
   wakeup_temp_environment();
+  delay(300);
 
   Serial.print("Temp: "); 
-  Serial.print(read_temp_environment(), 4); Serial.println("°C"); 
+  Serial.print(read_temp_environment(), 4); 
+  Serial.println("°C"); 
 
+  Serial.println("time:"); 
+  Serial.println(rtc.now().timestamp());
+
+  Serial.println("temp:"); 
+  Serial.println(rtc.getTemperature());
 
   delay(1000);
+  
 }
 
--- dataminer/src/main.h
+++ dataminer/src/main.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <Arduino.h>
+#include <ArduinoLowPower.h>
 #include <string.h>
+#include <RTCLib.h>
 #include "temp.h"
 #include "lora.h"
 #include "gps.h"
@@ -11,8 +13,10 @@
 #define VOLTAGE_METER_PIN A1  // Voltage meter signal
 #define CURRENT_METER_PIN A2  // Current meter signal
 
-#define TEMP_WAKE_TIME 70 // 70 * 10ms = 700ms to wakeup temp sensor 300 ms before reading
+#define SLEEPTIME 1000 //time controller can sleep, to have enaugh time to wake up temp sensor (300 ms is needed)
 #define LOOP_INTERVAL 10 // 10 * 100ms = 1s loop interval
+
+#define POSSIBLE_TIME 1000000000 // time at 2001, bigger the defult failur time from gps,  but still defitly wrong
 
 #define ADC_MAX 4095.0
 #define V_REF 3.3
@@ -30,8 +34,8 @@ extern uint32_t timestamp;
 struct Flags
 {
   uint8_t timer_interrupt : 1;  // flag for util timer
-  uint8_t pps_interrupt : 1;          // flag for PPS interrupt
-  uint8_t gps_connected : 1;    // flag for GPS connection
+  uint8_t pps_interrupt : 1;    // flag for PPS interrupt
+  uint8_t use_gps_time : 1;     // flag for GPS connection
 };
 
 // Declare the variable as extern
